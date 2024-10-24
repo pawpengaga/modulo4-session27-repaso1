01dfa1d15fe4ab6bd96e36511dd75e2b87d11429
Erick Rivera <yomellamoerick.771@gmail.com>
2024-10-24 20:20:24 -0300

    anotaciones sobre interfaces

--- src/main/java/modelo/Inmueble.java
+++ src/main/java/modelo/Inmueble.java
@@ -10,6 +10,10 @@ public class Inmueble {
   // El precio de venta
   protected double precioVenta;
 
+  // La interfaz no necesariamente tengo que implementarla en clases que esten relacionadas entre si
+  // Hay veces que en lo unico que dos clases van a tener en comun, es la interfaz
+  // Puedo separar acciones en interfaces y implementar las acciones en cuantas interfaces necesite por sujeto
+  // En las interfaces lo unico que esta es su definicion y lo que devuelven, no su implmentación
 
   public Inmueble(int idInmobiliario, double mtCuadrado, String direccion) {
     super();
